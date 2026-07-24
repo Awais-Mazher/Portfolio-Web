d3faa096b5ac3e8fe1b458577ea5a7b7472ab1a5
Awais-Mazher <mazherawais000@gmail.com>
2026-07-24 22:42:34 +0500

    Some more mobile adjustments

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className='social-icons'>
             <a href="https://www.linkedin.com/in/awais-mazher" target='_blank'><FaLinkedin className='icon linkedin' /></a>
             <a href="https://github.com/Awais-Mazher" target='_blank'><FaGithub className='icon github' /></a>
-            <a href="" target='_blank'><FaInstagramSquare className='icon instagram' /></a>
+            <a href="https://www.instagram.com/awais_mazher1?utm_source=qr&igsh=MWtydXA4d2F2cXRjbA%3D%3D" target='_blank'><FaInstagramSquare className='icon instagram' /></a>
         </div>
     </footer>
   )
